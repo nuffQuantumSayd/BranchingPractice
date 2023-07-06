3f343725b4ba910c1697097275038a26dddf1a47
nuffQuantumSayd <aaronkornish.ak@gmail.com>
2023-07-06 11:28:07 -0700

    Added a create student form

---
+++ BranchingPractice/CreateStudentForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BranchingPractice
+{
+	public partial class CreateStudentForm : Form
+	{
+		public CreateStudentForm()
+		{
+			InitializeComponent();
+		}
+	}
+}
